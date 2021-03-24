a66842a99d123ab9b52c4cb9d3ab557de9132de8
Tstegen1 <meiyeyoutai@gmail.com>
2021-03-24 20:14:06 +0900

    Vuex途中まで

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,32 @@
       </h1>
       <hr>
       <router-link to="/price">Price page</router-link>
+      <img src="~/assets/cat.jpg" alt="cat">
   </div>
+      <!-- <div>
+        <ul>
+          <li v-for="user in users" :key="user.id">
+            {{ user.id }},{{ user.name }}, {{ user.company.name }}
+          </li>
+        </ul>
+      </div> -->
 </template>
 
 <script>
+// const axios = require('axios');
+// let url = "https://jsonplaceholder.typicode.com/usersxxx";
+
 export default {
+  // asyncData({ params, error }) {
+  //   return axios.get(url)
+  //     .then((res) => {
+  //       return { users: res.data }
+  //     }) 
+  //     .catch((e) => {
+  //       console.log(e.message);
+  //       error({ statusCode: e.response.status, message: e.message })
+  //     })
+  // },
   data() {
     return {
       message: 'Helo World'
---
+++ store/index.js
@@ -0,0 +1,13 @@
+// import Vuex from 'vuex';
+
+// const createStore = () => {
+//   return new Vuex.Store({
+//     state: function() {
+//       return {
+//         message: 'hello Vuex!!'
+//       }
+//     }
+//   })
+// }
+
+// export default createStore
